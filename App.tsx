7779adc75194baff0814a42db919293048fe28ae
ViavaCos <ViavaCos@hotmail.com>
2024-05-20 17:26:55 +0800

    Add Route

--- App.tsx
+++ App.tsx
@@ -16,9 +16,10 @@ import {
   useColorScheme,
   View,
   Image,
+  TouchableOpacity,
 } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native'
-// import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import {
   Colors,
@@ -35,7 +36,16 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+
+//     setTimeout(() => {
+//         alert(JSON.stringify(props))
+//     }, 3000)
+
+    const handlePress = () => {
+        props.navigation.navigate("Mine", { mockData: 'mock~~' })
+    }
+
   return (
     <View>
       <Card showHeader={false}>
@@ -49,11 +59,22 @@ const HomeScreen = () => {
           <Image style={{ width: 150, height: 200, resizeMode: 'stretch' }} source={require('./src/assets/flower2.png')}></Image>
         </View>
       </Card>
+
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Mine</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-// const Stack = createNativeStackNavigator()
+const MineScreen = () => {
+    return (
+        <View><Text>Im</Text></View>
+    )
+}
+
+
+const Stack = createStackNavigator()
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -61,13 +82,6 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
-
-//             <NavigationContainer>
-//               <Stack.Navigator initialRouteName='Home'>
-//                 <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'i am home' }} />
-//               </Stack.Navigator>
-//             </NavigationContainer>
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -78,8 +92,12 @@ function App(): React.JSX.Element {
       <View
         style={{backgroundColor: 'pink', height: '100%', overflow: 'hidden'}}>
         <Layout>
-          <HomeScreen />
-
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home'>
+              <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'i am home' }} />
+              <Stack.Screen name='Mine' component={MineScreen} options={{ title: 'i am mine' }} />
+            </Stack.Navigator>
+          </NavigationContainer>
         </Layout>
         <NavBar />
       </View>
